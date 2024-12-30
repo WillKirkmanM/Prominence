a851f0eb9c70f8f1c1f756f50974054b36c3fedb
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-12-30 00:10:56 +0000

    Feat(boundary.js): Extend Boundary

--- scripts/boundary.js
+++ scripts/boundary.js
@@ -4,8 +4,14 @@ class Boundary {
     this.b = createVector(x2, y2);
   }
 
-  show() {
-    stroke(255);
-    line(this.a.x, this.a.y, this.b.x, this.b.y);
+    show() {
+      push();
+      beginShape();
+      vertex(this.a.x, this.a.y, this.a.z, 0, 0);
+      vertex(this.b.x, this.b.y, this.b.z, 1, 0);
+      vertex(this.b.x, this.b.y - 100, this.b.z, 1, 1);
+      vertex(this.a.x, this.a.y - 100, this.a.z, 0, 1);
+      endShape(CLOSE);
+      pop();
   }
 }
